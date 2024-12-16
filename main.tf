3522e1211a055fdd13025f16a0d406848dae2be1
Agatha Dantas <55562749+agathadantas@users.noreply.github.com>
2024-12-16 18:00:12 +0000

    Update candidate_ip main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ module "vpc" {
   cidr_block = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  candidate_ip = var.candidate_ip
 }
 
 module "s3" {
