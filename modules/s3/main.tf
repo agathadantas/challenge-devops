2d1ff505afb7e31020110a86f8274f04798dae42
Agatha Dantas <55562749+agathadantas@users.noreply.github.com>
2024-12-16 18:18:59 +0000

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  acl    = "private"
 
   lifecycle_rule {
     enabled = true
@@ -10,6 +9,11 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
   bucket = aws_s3_bucket.bucket.id
 
@@ -17,6 +21,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
