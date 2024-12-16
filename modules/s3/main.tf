fcf6a04a244f6fc1a70870d6d16041b076939035
Agatha Dantas <55562749+agathadantas@users.noreply.github.com>
2024-12-16 18:21:09 +0000

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,13 +1,20 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+}
 
-  lifecycle_rule {
-    enabled = true
+resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id     = "ExpireNonCurrentVersions"
+    status = "Enabled"
+
     noncurrent_version_expiration {
       days = 30
     }
   }
 }
+
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
