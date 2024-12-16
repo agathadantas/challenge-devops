d8972a51cc210208ad9df2aff12c5f6ae0e9abe1
Agatha Dantas <55562749+agathadantas@users.noreply.github.com>
2024-12-16 18:09:16 +0000

    Update outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -5,5 +5,5 @@ output "bucket_name" {
 
 output "bucket_versioning_status" {
   description = "Versioning status of the S3 bucket"
-  value       = aws_s3_bucket.bucket.versioning[0].status
+  value       = aws_s3_bucket_versioning.bucket_versioning.versioning_configuration[0].status
 }
